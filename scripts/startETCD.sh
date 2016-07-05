7375593737c83ab6977c65d191579280c0456d01
amoAHCP <amo.ahcp@gmail.com>
2016-07-05 21:46:31 +0200

    update script permissons

---
+++ scripts/startETCD.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker run -d -v /usr/share/ca-certificates/:/etc/ssl/certs -p 4001:4001 -p 2380:2380 -p 2379:2379 --name etcd quay.io/coreos/etcd -name etcd0 -advertise-client-urls http://:2379,http://:4001 -listen-client-urls http://0.0.0.0:2379,http://0.0.0.0:4001 -initial-advertise-peer-urls http://:2380 -listen-peer-urls http://0.0.0.0:2380 -initial-cluster-token etcd-cluster-1 -initial-cluster etcd0=http://:2380 -initial-cluster-state new
