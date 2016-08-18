0dacfc9a5f3670ab79934741d8d79cf7e6676a5a
amoAHCP <amo.ahcp@gmail.com>
2016-08-18 22:01:53 +0200

    minor fixes

--- vxms-service-discovery/src/main/java/org/jacpfx/vertx/registry/NodeResponse.java
+++ vxms-service-discovery/src/main/java/org/jacpfx/vertx/registry/NodeResponse.java
@@ -7,9 +7,12 @@ import java.util.Collections;
 import java.util.List;
 
 /**
+ * Represents a response for a service lookup
  * Created by Andy Moncsek on 12.05.16.
  */
 public class NodeResponse {
+    public static final String PORT_DELIMITER = ":";
+    public static final String PROTOCOL_DELIMITER = "://";
     private final List<Node> nodes;
     private final String domain;
     private final boolean succeeded;
@@ -22,25 +25,55 @@ public class NodeResponse {
         this.domain = domain;
     }
 
+    /**
+     * The service node provides access to a node instance
+     *
+     * @return the service node {@link ServiceNode}
+     */
     public ServiceNode getServiceNode() {
-        if(!succeeded) throw new NodeNotFoundException(throwable);
+        if (!succeeded) throw new NodeNotFoundException(throwable);
         Collections.shuffle(nodes);
         Node selectedNode = nodes.get(0);
-        NodeMetadata metadata = Json.decodeValue(selectedNode.getValue(),NodeMetadata.class);
-        URI uri =  URI.create(metadata.getProtocol()+"://"+metadata.getHost()+":"+metadata.getPort()+metadata.getPath());
-        return new ServiceNode(selectedNode.getKey(),metadata.getHost(),metadata.getPort(),metadata.isSecure(),uri,null);
+        NodeMetadata metadata = Json.decodeValue(selectedNode.getValue(), NodeMetadata.class);
+        URI uri = URI.create(metadata.getProtocol() + PROTOCOL_DELIMITER + metadata.getHost() + PORT_DELIMITER + metadata.getPort() + metadata.getPath());
+        return new ServiceNode(selectedNode.getKey(), metadata.getHost(), metadata.getPort(), metadata.isSecure(), uri, null);
     }
 
+    /**
+     * The node with all metadata to the service
+     *
+     * @return the {@link Node}
+     */
     public Node getNode() {
-        if(!succeeded) throw new NodeNotFoundException(throwable);
+        if (!succeeded) throw new NodeNotFoundException(throwable);
         return nodes.get(0);
     }
 
+    /**
+     * Check if discovery succeeded
+     *
+     * @return true if node was found
+     */
     public boolean succeeded() {
         return succeeded;
     }
 
+    /**
+     * in case of not succeeded you get the Throwable
+     *
+     * @return the error
+     */
     public Throwable getThrowable() {
         return throwable;
+    }
+
+    /**
+     * The domain name where the service is registered
+     *
+     * @return the service domain name
+     */
+    public String getDomain() {
+        if (!succeeded) throw new NodeNotFoundException(throwable);
+        return domain;
     }
 }
